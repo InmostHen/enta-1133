af128661442cd9c978075dabe863cc17a6be9977
InmostHen <henryshi1000@gmail.com>
2024-09-06 15:10:36 -0700

    Lab Assignment
    Computing Basics: Binary, Decimal, and Hexadecimal

--- GD12_1133_Lab1_Shi_Henry/GD12_1133_Lab1_Shi_Henry/Program.cs
+++ GD12_1133_Lab1_Shi_Henry/GD12_1133_Lab1_Shi_Henry/Program.cs
@@ -4,8 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Henry Shi ENTA 1133.");
+            Console.WriteLine("Hello, I'm Henry.");
+            
+            //Decimal
+            Console.WriteLine("0", 1, 2, 4, 8, 16, 32, 64, 100, 255);
+            //Binary
+            Console.WriteLine("0", 1, 10, 100, 1000, 10000, 100000, 1100100, 11111111);
+            //Hexadecimal
+            Console.WriteLine("0", 1, 2, 4, 8, 10, 20, 40, 64, "FF");
+
+            Console.WriteLine("Sept 6, 2024, I'll be leaving. Bye.");
+
         }
     }
 }
